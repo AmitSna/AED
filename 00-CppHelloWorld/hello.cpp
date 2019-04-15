5d234478b212978b437fb0df4f1554f470199bfa
AmitSna <18194916+AmitSna@users.noreply.github.com>
2019-04-15 08:54:48 -0300

    Descripción en tag extendida
    Adicionar información sobre precompiladores

--- 00-CppHelloWorld/hello.cpp
+++ 00-CppHelloWorld/hello.cpp
@@ -3,6 +3,10 @@ Trabajo #0: Hola Mundo!
 Descripción: Aplicación de Consola para mostrar string por pantalla
 Autor: Matías Salinas, Curso K1051
 Fecha: 07/04/2019 (v1.0)
+
+stdafx.h es un precompilador utilizado por el Visual Studio para
+abreviar el tiemp de compilación
+Fuente: https://stackoverflow.com/questions/4726155/what-is-stdafx-h-used-for-in-visual-studio/4726182#4726182
 */
 
 #include "stdafx.h"
@@ -16,7 +20,7 @@ int main()
 	std::cout << "Hola Mundo!\n";
 	std::cin.get();
 
-	//Escribir un string en un txt
+	//Escribir un string en la primera línea de un .txt
 	ofstream myfile;
 	myfile.open("output.txt");
 	myfile << "Hola Mundo!\n";
