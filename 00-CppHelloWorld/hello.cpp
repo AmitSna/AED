eba63c7f733191ccb645ac009af65a769a06270f
AmitSna <18194916+AmitSna@users.noreply.github.com>
2019-04-28 18:51:09 -0300

    Actualización del tag
    Corrección de typo

--- 00-CppHelloWorld/hello.cpp
+++ 00-CppHelloWorld/hello.cpp
@@ -5,7 +5,7 @@ Autor: Matías Salinas, Curso K1051
 Fecha: 07/04/2019 (v1.0)
 
 stdafx.h es un precompilador utilizado por el Visual Studio para
-abreviar el tiemp de compilación
+abreviar el tiempo de compilación
 Fuente 1: https://stackoverflow.com/questions/4726155/what-is-stdafx-h-used-for-in-visual-studio/4726182#4726182
 Fuente 2: https://stackoverflow.com/questions/37257725/why-do-we-use-include-stdafx-h-instead-of-include-stdafx-h
 */
