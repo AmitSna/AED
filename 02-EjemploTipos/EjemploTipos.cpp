5f01e4cdbd42ef8da90995eb5356b9626865a974
AmitSna <18194916+AmitSna@users.noreply.github.com>
2019-05-01 23:49:13 -0300

    Actualización Tags
    Actualización de descripción de Ejercicios Extra 1 y 2.

--- 02-EjemploTipos/EjemploTipos.cpp
+++ 02-EjemploTipos/EjemploTipos.cpp
@@ -1,11 +1,11 @@
 /*
 Trabajo #02: Ejemplos de Valores y Operaciones de Tipos de Datos
-Descripción: Aplicación de Consola para realizar operaciones entre tipos de datos y su aplicación,
+DescripciÃ³n: AplicaciÃ³n de Consola para realizar operaciones entre tipos de datos y su aplicaciÃ³n,
 a modo de ejemplo entre los mismos (bool, unsigned, int, double, char y string).
-Autor: Matías Salinas, Curso K1051
+Autor: MatÃ­as Salinas, Curso K1051
 Fecha: 01/05/2019 (v1.0)
 stdafx.h es un Precompilador utilizado por el Visual Studio
-para abreviar el tiempo de compilación
+para abreviar el tiempo de compilaciÃ³n
 Fuente 1: https://stackoverflow.com/questions/4726155/what-is-stdafx-h-used-for-in-visual-studio/4726182#4726182
 Fuente 2: https://stackoverflow.com/questions/37257725/why-do-we-use-include-stdafx-h-instead-of-include-stdafx-h
 */
@@ -18,50 +18,50 @@ using namespace std;
 void main()
 {
 	bool bTrue = true, bFalse = false; //Incluye los valores true y false
-	std::cout << bTrue && bFalse; //Permite Conjunción
-	std::cout << bFalse || bFalse; //Permite Disyunción
-	std::cout << !bFalse << "\n"; //Permite Negación Lógica
-	bTrue = 1; std::cout << bTrue << "\n"; //Permite Valores numéricos, los cuales dan true
-	bFalse = 0; std::cout << bFalse << "\n"; //Permite Valores numéricos, si es 0 da false
-	std::cout << bTrue + bFalse << "\n"; //Permite Adición
+	std::cout << bTrue && bFalse; //Permite ConjunciÃ³n
+	std::cout << bFalse || bFalse; //Permite DisyunciÃ³n
+	std::cout << !bFalse << "\n"; //Permite NegaciÃ³n LÃ³gica
+	bTrue = 1; std::cout << bTrue << "\n"; //Permite Valores numÃ©ricos, los cuales dan true
+	bFalse = 0; std::cout << bFalse << "\n"; //Permite Valores numÃ©ricos, si es 0 da false
+	std::cout << bTrue + bFalse << "\n"; //Permite AdiciÃ³n
 	std::cout << bTrue - bTrue << "\n"; //Permite Diferencia
 	bTrue = 1 > 2; std::cout << bTrue << "\n\n";
-	/*El resultado de una comparación lógica entre valores numéricos o de caracteres
-	(<, >, <=, <=, == y !=) retorna un bool. Como el ejemplo contenía un valor numérico,
+	/*El resultado de una comparaciÃ³n lÃ³gica entre valores numÃ©ricos o de caracteres
+	(<, >, <=, <=, == y !=) retorna un bool. Como el ejemplo contenÃ­a un valor numÃ©rico,
 	devuelve 0 lo que equivale a false en boolean.*/
 
 	unsigned uA = 6, uB = 3; //Incluye el Conjunto N ^ {0}
-	std::cout << uA + uB << "\n"; //Permite Adición
+	std::cout << uA + uB << "\n"; //Permite AdiciÃ³n
 	std::cout << uA - uB << "\n"; //Permite Diferencia en Enteros
 	std::cout << uA * uB << "\n"; //Permite Producto
-	std::cout << uA / uB << "\n"; //Permite División Entera
-	bTrue = uA > uB; std::cout << bTrue << "\n\n"; //Permite Comparación Lógica
+	std::cout << uA / uB << "\n"; //Permite DivisiÃ³n Entera
+	bTrue = uA > uB; std::cout << bTrue << "\n\n"; //Permite ComparaciÃ³n LÃ³gica
 
 	int iA = -2, iB = 12; //Incluye el Conjunto Z
-	std::cout << iA + iB << "\n"; //Permite Adición
+	std::cout << iA + iB << "\n"; //Permite AdiciÃ³n
 	std::cout << iA - iB << "\n"; //Permite Diferencia
 	std::cout << iA * iB << "\n"; //Permite Producto
-	std::cout << iA / iB << "\n"; //Permite División en Z+
-	bTrue = iA == iB; std::cout << bTrue << "\n\n"; //Permite Comparación Lógica
+	std::cout << iA / iB << "\n"; //Permite DivisiÃ³n en Z+
+	bTrue = iA == iB; std::cout << bTrue << "\n\n"; //Permite ComparaciÃ³n LÃ³gica
 
 	double dA = 2.3, dB = 5.4; //Incluye el Conjunto Q
-	std::cout << dA + dB << "\n"; //Permite Adición
+	std::cout << dA + dB << "\n"; //Permite AdiciÃ³n
 	std::cout << dA - dB << "\n"; //Permite Diferencia
 	std::cout << dA * dB << "\n"; //Permite Producto
-	std::cout << dA / dB << "\n"; //Permite División Fraccional
-	bTrue = dA != dA; std::cout << bTrue << "\n\n"; //Permite Comparación Lógica	
+	std::cout << dA / dB << "\n"; //Permite DivisiÃ³n Fraccional
+	bTrue = dA != dA; std::cout << bTrue << "\n\n"; //Permite ComparaciÃ³n LÃ³gica	
 
 	char cA = 'A', cB = '5';
-	std::cout << cA++ << "\n"; //Permite Adición, lo cual modifica el valor ASCII contenido
+	std::cout << cA++ << "\n"; //Permite AdiciÃ³n, lo cual modifica el valor ASCII contenido
 	std::cout << cA-- << "\n"; //Permite Diferencia, lo cual modifica el valor ASCII contenido
-	std::cout << cA + cB + cA + cB + cB << "\n"; //Permite Adición entre varios char
-	bTrue = cA > cB; std::cout << bTrue << "\n\n"; //Permite Comparación Lógica
+	std::cout << cA + cB + cA + cB + cB << "\n"; //Permite AdiciÃ³n entre varios char
+	bTrue = cA > cB; std::cout << bTrue << "\n\n"; //Permite ComparaciÃ³n LÃ³gica
 	
 
 	//El string se crea como objeto en lugar de como tipo de dato primitivo
-	string sA = "Alfabéticas", sB = "012345";
-	string sC = sA + sB; //Permite Concatenación
-	bTrue = sA == sB; std::cout << bTrue; //Permite Comparación Lógica
+	string sA = "AlfabÃ©ticas", sB = "012345";
+	string sC = sA + sB; //Permite ConcatenaciÃ³n
+	bTrue = sA == sB; std::cout << bTrue; //Permite ComparaciÃ³n LÃ³gica
 
 
 	std::cin.get();
@@ -69,31 +69,32 @@ void main()
 
 /*
 Bonus 1:
-Adicionalmente, hay otros tipos de datos más abstractos que aún no hemos visto en clase,
-por lo cual los aquí presentados no son todos los tipos de datos existentes en C++.
+AdemÃ¡s de los mencionados, hay otros tipos de datos mÃ¡s abstractos que aÃºn no hemos visto en clase,
+por lo cual los aquÃ­ presentados no son todos los tipos de datos existentes en C++, como por ejemplo:
+String, Date and Time, Money/Currency/Monetary value (<iomanip>), NÃºmeros Complejos y NÃºmeros Compuestos.
 Adicionalmente, el Object Stream (para input std::cin y output std::cout) es un tipo
-de dato visto en clase pero no contemplado en la demostración, el cual a su vez tiene
+de dato visto en clase pero no contemplado en la demostraciÃ³n, el cual a su vez tiene
 su propio conjunto de operaciones con >> (para input) y << (para output) y se utiliza
-para recibir valores por medio de interacción del usuario y mostrar otros valores por pantalla.
-También, otros datos primitivos no vistos en clase aún pueden ser:
+para recibir valores por medio de interacciÃ³n del usuario y mostrar otros valores por pantalla.
+TambiÃ©n, otros datos primitivos no vistos en clase aÃºn pueden ser:
 float, void, wchar_t, unsigned char, signed char, unsigned int, signed int, short int,
 unsigned short int, signed short int, long int, signed long int, unsigned long int,
-long double
+long double.
 */
 
 /*
 Bonus 2:
-Sin utilizar cout y utilizando assert, ejemplos a continuación:
-Modificar el nombre de la Función a main y ejecutar!!
+Sin utilizar cout y utilizando assert, ejemplos a continuaciÃ³n:
+Modificar el nombre de la FunciÃ³n a main y ejecutar!!
 */
 
 void main2() {
 	int iUno = 1, iDos = 2, iTres = 3;
 
-	assert(iUno != iDos); //Completa la instrucción y continúa		
+	assert(iUno != iDos); //Completa la instrucciÃ³n y continÃºa		
 	std::cin.get();
 
-	assert(iDos == 2); //Completa la instrucción y continúa
+	assert(iDos == 2); //Completa la instrucciÃ³n y continÃºa
 	std::cin.get();
 
 	assert(iTres < 0); //Devuelve error y cierra el programa
@@ -103,12 +104,12 @@ void main2() {
 
 	bool bTrue = true, bFalse = false;
 
-	assert(!bFalse); //Completa la instrucción y continúa
+	assert(!bFalse); //Completa la instrucciÃ³n y continÃºa
 	std::cin.get();
 
 
 
-	char cEszett = 'ß';
-	assert('ß' == cEszett); //Completa la instrucción y continúa
+	char cEszett = 'ÃŸ';
+	assert('ÃŸ' == cEszett); //Completa la instrucciÃ³n y continÃºa
 	std::cin.get();
 }
